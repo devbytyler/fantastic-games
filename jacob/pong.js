8f9202468702acf01b308afa9b0675c44c11c176
Jacob Ashcraft <jacobashcraft@Jareds-MacBook-Air.local>
2020-04-12 14:24:15 -0600

    soeed update

--- jacob/pong.js
+++ jacob/pong.js
@@ -54,7 +54,7 @@ function Ball(x, y) {
     this.x = x;
     this.y = y;
     this.x_speed = 0;
-    this.y_speed = 3;
+    this.y_speed = 4.5;
     this.radius = 5;
 }
 
@@ -107,7 +107,7 @@ Ball.prototype.update = function(paddle1, paddle2) {
 
   if(this.y < 0 || this.y > 600) { // a point was scored
     this.x_speed = 0;
-    this.y_speed = 3;
+    this.y_speed = 4.5;
     this.x = 200;
     this.y = 300;
   }
@@ -115,14 +115,14 @@ Ball.prototype.update = function(paddle1, paddle2) {
   if(top_y > 300) {
     if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
       // hit the player's paddle
-      this.y_speed = -3;
+      this.y_speed = -4.5;
       this.x_speed += (paddle1.x_speed / 2);
       this.y += this.y_speed;
     }
   } else {
     if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
       // hit the computer's paddle
-      this.y_speed = 3;
+      this.y_speed = 4.5;
       this.x_speed += (paddle2.x_speed / 2);
       this.y += this.y_speed;
     }
@@ -148,9 +148,9 @@ Player.prototype.update = function() {
   for(var key in keysDown) {
     var value = Number(key);
     if(value == 37) { // left arrow
-      this.paddle.move(-4, 0);
+      this.paddle.move(-6, 0);
     } else if (value == 39) { // right arrow
-      this.paddle.move(4, 0);
+      this.paddle.move(6, 0);
     } else {
       this.paddle.move(0, 0);
     }
@@ -181,9 +181,9 @@ Computer.prototype.update = function(ball) {
   var x_pos = ball.x;
   var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
   if(diff < 0 && diff < -4) { // max speed left
-    diff = -5;
+    diff = -7;
   } else if(diff > 0 && diff > 4) { // max speed right
-    diff = 5;
+    diff = 7;
   }
   this.paddle.move(diff, 0);
   if(this.paddle.x < 0) {
