4b75b46247a72f62b0103e17eaef0b60f0efaf7c
tylerstephens814 <tylerstephens814@gmail.com>
2020-04-19 15:59:32 -0600

    Refactor homepage with vue

---
+++ main.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        games: [
+            {
+                name: 'Super Brick', 
+                desc: 'A classic brick-breaking adventure.', 
+                img: 'images/brick.jpg', 
+                link: 'Nathan-Game/superBrick.html', 
+                creator:'Nathan',
+            },
+            {
+                name: 'Goalie Wars', 
+                desc: "A super addicting pong adventure. Don't worry if you can't beat it. I can't either.", 
+                img: 'images/pong.jpg', 
+                link: 'jacob/game.html', 
+                creator:'Jacob',
+            },
+            {
+                name: 'Escape Room', 
+                desc: 'An amazing online escape room.', 
+                img: 'images/escapeRoom.jpeg', 
+                link: 'escapeRoom/main-page.html', 
+                creator: 'Nathan & Jacob',
+            },
+            {
+                name: 'Coming Soon...', 
+                desc: 'Wait for it!', 
+                img: 'https://picsum.photos/200/100', 
+                link: '#', 
+                creator: 'Tyler',
+            },
+        ]
+    }
+})
