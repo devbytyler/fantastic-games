2a3b80204b356fc9b875dabc4deb6331d80f97de
nathanashcraft <nathancashcraft@gmail.com>
2020-06-25 17:03:16 -0600

    fixed boss again

--- Nathan-Game/spaceShooter/spaceShooter.js
+++ Nathan-Game/spaceShooter/spaceShooter.js
@@ -768,7 +768,7 @@ function hitEnemy(enemy, bullet) {
     }
 
     //  Launch boss
-    if (!bossLaunched && score > 1500) {
+    if (!bossLaunched && score > 15000) {
         greenEnemySpacing = 5000;
         blueEnemySpacing = 12000;
         redEnemySpacing = 12000;
